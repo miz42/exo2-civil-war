6352b22451d0405da9f5d361486b61d33904571e
MZZ <m.zwerenz@epiconcept.fr>
2017-04-26 14:33:02 +0200

    Ajouts

--- KaiserSoze/Factory.php
+++ KaiserSoze/Factory.php
@@ -5,6 +5,18 @@ class Core_Library_Export_Factory
 		switch ($sExtension) {
 			case 'csv' :
 				return new Core_Library_Export_CsvBuilder();
+			case 'csv' :
+				return new Core_Library_Export_CsvBuilder2();
+			case 'dta' :
+				return new Core_Library_Export_DtaBuilder();
+			case 'gss' :
+				return new Core_Library_Export_GssBuilder();
+			case 'xls' :
+				return new Core_Library_Export_ExcelBuilder( 'xls');
+			case 'xlsx' :
+				return new Core_Library_Export_ExcelBuilder('xlsx');
+			case 'codebook' :
+				return new Core_Library_Export_CodebookBuilder();
 			case 'ods' :
 				return null;
 			default :
