b08816110661c4c3f374f02a1a61c5f7c984f6af
MZZ <m.zwerenz@epiconcept.fr>
2017-04-26 14:31:20 +0200

    Debugs / indentation / ajouts

--- KaiserSoze/Factory.php
+++ KaiserSoze/Factory.php
@@ -2,9 +2,14 @@ class Core_Library_Export_Factory
 {
 	public static function getBuilderFor($sExtension = 'csv')
 	{
-switch ($sExtension) {
+		switch ($sExtension) {
 			case 'csv' :
 				return new Core_Library_Export_CsvBuilder();
+			case 'ods' :
+				return null;
+			default :
+				return null
+		}
 	}
 }
 
