94471119d2b31b28162fd9e91082573ea54eb0d2
MZZ <m.zwerenz@epiconcept.fr>
2017-04-26 16:54:32 +0200

    Exercice cherry-pick

---
+++ Frankenstein/Creature.php
@@ -0,0 +1,27 @@
+<?php
+
+function head()
+{
+    echo "1 tête\n";
+}
+
+function chest(){}
+function left_arm(){}
+function right_arm(){}
+function abdomen(){}
+function left_leg(){}
+function right_leg(){}
+
+function its_alive(){
+	head();
+	chest();
+	left_arm();
+	right_arm();
+	abdomen();
+	left_leg();
+	right_leg();
+}
+
+its_alive();
+
+?>
